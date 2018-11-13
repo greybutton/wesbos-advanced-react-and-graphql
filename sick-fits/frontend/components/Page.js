ab430c5fb8ca65a44bb82fc611cfb52d102a62f2
greybutton <greybutton@yandex.com>
2018-11-13 11:03:17 +0800

    module #2. An Intro to Styled Components

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,11 +1,28 @@
+import styled from 'styled-components';
 import Header from './Header';
 import Meta from './Meta';
+
+const MyButton = styled.button`
+  background: red;
+  font-size: ${props => (props.huge ? '30px': '10px')};
+  span {
+    font-size: 20px;
+  }
+`;
 
 const Page = (props) => {
   return (
     <div>
       <Meta />
       <Header />
+      <MyButton huge>
+        Click me
+        <span>icon</span>
+      </MyButton>
+      <MyButton>
+        Click me
+        <span>icon</span>
+      </MyButton>
       {props.children}
     </div>
   );
