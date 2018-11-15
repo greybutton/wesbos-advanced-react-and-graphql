d81bb7fc065516a65e3b7cf5d0322497f898ea1c
greybutton <greybutton@yandex.com>
2018-11-15 10:41:05 +0800

    module #2. Themes and Layout with Styled Components

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,6 +1,27 @@
-import styled from 'styled-components';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
 import Header from './Header';
 import Meta from './Meta';
+
+const theme = {
+  red: '#FF0000',
+  black: '#393939',
+  grey: '#3A3A3A',
+  lightgrey: '#E1E1E1',
+  offWhite: '#EDEDED',
+  maxWidth: '1000px',
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
+};
+
+const StyledPage = styled.div`
+  background: white;
+  color: ${props => props.theme.black};
+`;
+
+const Inner = styled.div`
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+  padding: 2rem;
+`;
 
 const MyButton = styled.button`
   background: red;
@@ -12,19 +33,21 @@ const MyButton = styled.button`
 
 const Page = (props) => {
   return (
-    <div>
-      <Meta />
-      <Header />
-      <MyButton huge>
-        Click me
-        <span>icon</span>
-      </MyButton>
-      <MyButton>
-        Click me
-        <span>icon</span>
-      </MyButton>
-      {props.children}
-    </div>
+    <ThemeProvider theme={theme}>
+      <StyledPage>
+        <Meta />
+        <Header />
+        <MyButton huge>
+          Click me
+          <span>icon</span>
+        </MyButton>
+        <MyButton>
+          Click me
+          <span>icon</span>
+        </MyButton>
+        <Inner>{props.children}</Inner>
+      </StyledPage>
+    </ThemeProvider>
   );
 };
 
