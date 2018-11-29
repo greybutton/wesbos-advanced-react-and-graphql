b37dc577c5d23f4efb3ba629179ded8274f94fc4
greybutton <greybutton@yandex.com>
2018-11-29 14:13:46 +0800

    module #3. Items Creation and Prisma Yoga Flow

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,9 +1,14 @@
 const Mutations = {
-  createDog: (parent, args, ctx, info) => {
-    global.dogs = global.dogs || [];
-    const newDog = { name: args.name };
-    global.dogs.push(newDog);
-    return newDog;
+  createItem: async (parent, args, ctx, info) => {
+    const item = await ctx.db.mutation.createItem(
+      { 
+        data: { 
+          ...args
+        }
+      }, 
+      info)
+    ;
+    return item;
   }
 };
 
