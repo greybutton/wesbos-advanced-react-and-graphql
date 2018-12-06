d0b9437e74c406c93d8235ae632edd8518d7ad23
greybutton <greybutton@yandex.com>
2018-12-06 14:02:18 +0800

    module #5. User Signup and Permission Flow

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutations = {
   createItem: async (parent, args, ctx, info) => {
     const item = await ctx.db.mutation.createItem(
@@ -26,6 +29,28 @@ const Mutations = {
     const where = { id: args.id };
     const item = await ctx.db.query.item({ where }, `{ id title }`);
     return ctx.db.mutation.deleteItem({ where }, info);
+  },
+  signup: async (_, args, ctx, info) => {
+    const { email, password } = args;
+    console.log(args)
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          email: email.toLowerCase(),
+          password: hashedPassword,
+          permissions: { set: ['USER'] },
+        }
+      },
+      info
+    );
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year
+    });
+    return user;
   }
 };
 
