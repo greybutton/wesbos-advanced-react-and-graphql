4cd1c3263eeed12c0d0fda64b15fa2ba56bcb33d
greybutton <greybutton@yandex.com>
2018-12-07 11:18:28 +0800

    module #5. Currently Logged In User with Middleware and Render Props

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -4,6 +4,20 @@ const Query = {
   items: forwardTo('db'),
   item: forwardTo('db'),
   itemsConnection: forwardTo('db'),
+  me: (_, args, ctx, info) => {
+    const { userId } = ctx.request;
+    if (!userId) {
+      return null;
+    }
+    return ctx.db.query.user(
+      {
+        where: {
+          id: userId,
+        },
+      },
+      info
+    );
+  }
   // items: async (parent, args, ctx, info) => {
   //   const items = await ctx.db.query.items();
   //   return items;
--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,9 +1,16 @@
 import Link from 'next/link';
 import NavStyles from './styles/NavStyles';
+import User from './User';
 
 const Nav = () => {
   return (
     <NavStyles>
+      <User>
+        {({ data: { me } }) => {
+          if (me) return <p>{me.name}</p>
+          return null;
+        }}
+      </User>
       <Link href="/items">Shop</Link>
       <Link href="/sell">Sell</Link>
       <Link href="/signup">Signup</Link>
