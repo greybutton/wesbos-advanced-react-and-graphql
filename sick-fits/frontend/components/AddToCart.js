4b17150d03af3f0a2e1c134271b817112a833225
greybutton <greybutton@yandex.com>
2018-12-12 14:12:33 +0800

    module #6. Displaying Cart Items and Totals

--- sick-fits/frontend/components/AddToCart.js
+++ sick-fits/frontend/components/AddToCart.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
+import { CURRENT_USER_QUERY } from './User';
 
 const ADD_TO_CART_MUTATION = gql`
   mutation addToCart($id: ID!) {
@@ -18,11 +19,13 @@ class AddToCart extends React.Component {
       <Mutation
         mutation={ADD_TO_CART_MUTATION}
         variables={{ id }}
+        refetchQueries={[{ query: CURRENT_USER_QUERY }]}
       >
-        {addToCart => (
+        {(addToCart, { loading }) => (
           <button
             type="button"
             onClick={addToCart}
+            disabled={loading}
           >
             Add to cart
           </button>
