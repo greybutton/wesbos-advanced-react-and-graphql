8b3c5c1719e93427f2bfc962bd1e7d3f8477ddc8
greybutton <greybutton@yandex.com>
2018-12-12 10:34:05 +0800

    module #6. Server Side Add To Cart

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -190,6 +190,46 @@ const Mutations = {
       },
       info
     );
+  },
+  addToCart: async (_, args, ctx, info) => {
+    const { userId } = ctx.request;
+    if (!userId) {
+      throw new Error('You must be logged in!');
+    }
+    const [existingCartItem] = await ctx.db.query.cartItems(
+      {
+        where: {
+          user: { id: userId },
+          item: { id: args.id },
+        }
+      }
+    );
+    if (existingCartItem) {
+      return ctx.db.mutation.updateCartItem(
+        {
+          where: { id: existingCartItem.id },
+          data: { quantity: existingCartItem.quantity + 1 }
+        },
+        info
+      );
+    }
+    return ctx.db.mutation.createCartItem(
+      {
+        data: {
+          user: {
+            connect: {
+              id: userId
+            }
+          },
+          item: {
+            connect: {
+              id: args.id
+            }
+          }
+        }
+      },
+      info
+    );
   }
 };
 
